bf13c98560ea3ebd60f0afb09f4bbe10f4fa0ede
Jonathon Richardson <jonathon.m.richardson@gmail.com>
2022-11-25 22:46:32 -0600

    Add some commentary on commented code.

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,11 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(s?css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
+  // For some reason, projects don't seem to work, but they also don't seem to be necessary, so leaving
+  // this commented out for now.  It would probably be ideal to run the tests in project batches if we
+  // were actually publishing them individually.
+  // 
+  // See here for more info: https://github.com/kulshekhar/ts-jest/issues/1648
   // projects: [
   //   '<rootDir>/lib/ts/packages/config'
   // ],

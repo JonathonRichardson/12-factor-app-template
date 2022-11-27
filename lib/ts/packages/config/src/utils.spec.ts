adfe0c85a39cbbe0afe78ac0d2349c6b2c4713dc
Jonathon Richardson <jonathon.m.richardson@gmail.com>
2022-11-27 01:28:07 -0600

    Add Debug Configuration for Jest

--- lib/ts/packages/config/src/utils.spec.ts
+++ lib/ts/packages/config/src/utils.spec.ts
@@ -1,10 +1,12 @@
 import { cx } from "./utils";
 
-test("cx concats class names", () => {
-    expect(cx("a", "b", "c")).toBe("a b c");
-});
+describe("cx", () => {
+    test("concats class names", () => {
+        expect(cx("a", "b", "c")).toBe("a b c");
+    });
 
-test("cx ignores false, null, and undefined values", () => {
-    const classNames = cx("a", false && "b", null, undefined, "c d");
-    expect(classNames).toBe("a c d");
+    test.only("ignores false, null, and undefined values", () => {
+        const classNames = cx("a", false && "b", null, undefined, "c d");
+        expect(classNames).toBe("a c d");
+    });
 });
